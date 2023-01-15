6f0897c44ea1a705b6f85e19d75d20e0b3f43b67
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-15 23:06:31 +0100

    [Update :rocket:] - Make file executable

--- markdown2html.py
+++ markdown2html.py
@@ -3,7 +3,7 @@
     Scripts takes two arguments:
     - Name of markdown file
     - Output file name
-    Return:
+    Return
     - Exit with 1 if arguments < 2
     - Exit with 1 if file not found
     - Exit with 0 if success.
@@ -14,13 +14,13 @@ import os
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(
-            'Usage: ./markdown2html.py README.md README.html',
-            file=sys.stderr
-        )
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
         exit(1)
+
     if not os.path.isfile(sys.argv[1]):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
+
     else:
         exit(0)
