ee06102c440b0c21b9242c098c14e2e52a8e77b7
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-20 15:23:32 +0100

    [Fix :dart:] - fix markdown2html code

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-''' Write a script markdown2html.py that takes an argument 2 strings:
-
-    First argument is the name of the Markdown file
-    Second argument is the output file name
-'''
-
-import sys
+''' parses markdown to html '''
 import os
+import sys
 
 if __name__ == '__main__':
+
     if len(sys.argv) < 3:
         print('Usage: ./markdown2html.py README.md README.html',
               file=sys.stderr)
         exit(1)
-
-    if not os.path.isfile(sys.argv[1]):
+    if os.path.isfile(sys.argv[1]) == False:
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
-    exit(0)
+    else:
+        print()
+        exit(0)
